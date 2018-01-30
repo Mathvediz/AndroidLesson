86261d78a2521089b44e3e882e3ef6c942cf8f23
Mathvediz <32832431+Mathvediz@users.noreply.github.com>
2018-01-30 11:16:03 +0800

    Initial commit

---
+++ app/src/main/java/net/timedoor/androidlesson/TopActivity.java
@@ -0,0 +1,13 @@
+package net.timedoor.androidlesson;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top);
+    }
+}
